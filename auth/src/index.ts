009ad2daf89edb709b0eb2260c98bb942490a402
Razvan <razvan.rauta@gmail.com>
2020-10-03 21:44:47 +0300

    Error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import 'express-async-errors'
 import { json } from 'body-parser'
 
 import { currentUserRouter } from './routes/current-user'
@@ -6,6 +7,7 @@ import { signInRouter } from './routes/signin'
 import { signOutRouter } from './routes/signout'
 import { signUpRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(json())
@@ -14,5 +16,11 @@ app.use(currentUserRouter)
 app.use(signInRouter)
 app.use(signOutRouter)
 app.use(signUpRouter)
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
 
 app.listen(3000, () => console.log('Listening on port 3000!'))

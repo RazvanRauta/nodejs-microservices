446a782fad4ce2e4b773fa1c84fb40615de2339e
Razvan <razvan.rauta@gmail.com>
2020-10-04 13:07:30 +0300

    Added jwt session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 
 import { currentUserRouter } from './routes/current-user'
 import { signInRouter } from './routes/signin'
@@ -11,7 +12,15 @@ import { errorHandler } from './middlewares/error-handler'
 import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
+app.set('trust proxy', true)
 app.use(json())
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+)
 
 /**
  *
@@ -45,6 +54,10 @@ app.use(errorHandler)
  *
  */
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY is not defined')
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,41 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
+
+import { RequestValidationError } from '../errors/request-validation-error'
+import { User } from '../models/user'
+import { Password } from '../services/password'
 
 const router = express.Router()
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hello from sign in')
-})
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array())
+    }
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      if (Password.compare(existingUser.password, password)) {
+        res.send('Hello from sign in')
+      } else {
+        res.status(400).send('Wrong Password')
+      }
+    } else {
+      res.status(400).send("Wrong E-mail or user doesn't exist")
+    }
+  }
+)
 
 export { router as signInRouter }

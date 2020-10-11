eaf95693e1287583576bdca04c4bd28910d19720
Razvan <razvan.rauta@gmail.com>
2020-10-11 16:41:13 +0300

    Added Jest and set up first test

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,13 @@
+import supertest from 'supertest'
+import request from 'supertest'
+import { app } from '../../app'
+
+it('returns a 201 on successful signUp', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+})

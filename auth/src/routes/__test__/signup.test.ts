7527cf73a3327e62997d0cc61f1bbb83f92364b8
Razvan <razvan.rauta@gmail.com>
2020-10-11 18:23:41 +0300

    Added tests for all routes

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,4 +1,4 @@
-import supertest from 'supertest'
+import { header } from 'express-validator'
 import request from 'supertest'
 import { app } from '../../app'
 
@@ -10,4 +10,69 @@ it('returns a 201 on successful signUp', async () => {
       password: 'password',
     })
     .expect(201)
+})
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtestcom',
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p',
+    })
+    .expect(400)
+})
+
+it('returns a 400 with missing email and pass', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+    })
+    .expect(400)
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('disallow duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('sets cookie after a successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+
+  expect(response.get('Set-Cookie')).toBeDefined()
 })

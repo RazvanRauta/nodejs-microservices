d43af2b540065bbfc98002e3d63e6116b160dbb6
Razvan <razvan.rauta@gmail.com>
2021-03-07 23:07:59 +0300

    Lecture 222

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,5 +1,34 @@
-const Index = () => {
-    return <h1>Landing pages</h1>
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
+    // console.log(currentUser);
+    // axios.get('/api/users/currentuser');
+    console.log(currentUser)
+
+    return <h1>Landing Page</h1>
 }
 
-export default Index
+LandingPage.getInitialProps = async () => {
+    if (typeof window === 'undefined') {
+        // we are on the server!
+        // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                headers: {
+                    Host: 'ticketing.dev',
+                },
+            }
+        )
+
+        return data
+    } else {
+        // we are on the browser!
+        // requests can be made with a base url of ''
+        const { data } = await axios.get('/api/users/currentuser')
+
+        return data
+    }
+}
+
+export default LandingPage
